6656d506a4cc737eb14a69c0a0e99439d4103609
mixolydia <ronaldchanou@gmail.com>
2015-05-17 01:17:25 -0700

    broken carousel example

--- demo/src/carousel.jsx
+++ demo/src/carousel.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 import O from '../../oomph';
 
 
+const slides = ['aaa','bbb','ccc','ddd'];
+
+const slideNodes = slides.map(s => <O wF={1} hF={1} d='row' key={s}>
+  {s}
+</O>);
+
 export default class Carousel extends React.Component {
 
   render(){
     return <O>
       <O e='button'>{'<'}</O>
 
-      <O wF={17/19}>
-        tsrtsratsr
+      <O wF={17/19} hF={1}>
+        <O wF={4}>
+          {slideNodes}
+        </O>
       </O>
 
       <O e='button'>{'>'}</O>
--- oomph.jsx
+++ oomph.jsx
@@ -86,10 +86,14 @@ export default class FlexO extends React.Component {
       newStyle.height = this.state.rootHeight * (hF || 1);
       newStyle.width = this.state.rootWidth * (wF || 1);
     } else {
-      if (1 || wF){
+      if (0 || wF > 1){
+        newStyle.width = 1000;
+      } else {
         newStyle.width = `${(wF || 1) * 100}%`;
       }
-      if (1 || hF){
+      if (0 || hF > 1){
+        newStyle.height = '100%';
+      } else {
         if (this.props.test){ console.log('test hF', `${(hF || 1) * 100}%`); }
         newStyle.height = `${(hF || 1) * 100}%`;
       }
@@ -141,10 +145,8 @@ export default class FlexO extends React.Component {
     hLeft = Math.max(hLeft, 0);
 
     var kidCount = React.Children.count(children);
-
-
-    var autoKidWF = noWFCount && newStyle.flexDirection.startsWith('row') && wLeft;
-    var autoKidHF = noHFCount && newStyle.flexDirection.startsWith('col') && hLeft;
+    //var autoKidWF = noWFCount && newStyle.flexDirection.startsWith('row') && wLeft;
+    //var autoKidHF = noHFCount && newStyle.flexDirection.startsWith('col') && hLeft;
     //if (autoKidWF && autoKidHF){ console.log('wwwww'); }
 
     var totalCWF = 0, totalCHF = 0;
@@ -167,8 +169,31 @@ export default class FlexO extends React.Component {
         //console.log(autoKidWF, autoKidHF, kidCount);
 
 
-        newProps.wF = c.props.wF || cWF  || (autoKidWF? (wLeft/noWFCount): 1);
-        newProps.hF = c.props.hF || cHF  || (autoKidHF? (hLeft/noHFCount): 1);
+        newProps.wF = c.props.wF || cWF;//  || (autoKidWF? (wLeft/noWFCount): 1);
+        newProps.hF = c.props.hF || cHF;//  || (autoKidHF? (hLeft/noHFCount): 1);
+
+        if (!newProps.wF){
+          if (newStyle.flexDirection.startsWith('col')){
+            newProps.wF = 1;
+          } else {
+            if (1 || wLeft > 0.00001){
+              newProps.wF = wLeft/noWFCount;
+            } else {
+              newProps.wF = 1/kidCount;
+            }
+          }
+        }
+        if (!newProps.hF){
+          if (newStyle.flexDirection.startsWith('col')){
+            if (1 || hLeft > 0.00001){
+              newProps.hF = hLeft/noHFCount;
+            } else {
+              newProps.hF = 1/kidCount;
+            }
+          } else {
+            newProps.hF = 1;
+          }
+        }
 
         /*if (autoKidWF && !autoKidHF){
           console.log(autoKidWF? 'auto w': '', autoKidHF? 'auto h': '');
